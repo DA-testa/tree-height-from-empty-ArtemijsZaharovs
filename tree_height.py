6e1891d531bccad1ad954dffd211ba9fbe4db6db
ArtemijsZaharovs <124812244+ArtemijsZaharovs@users.noreply.github.com>
2023-03-05 21:35:26 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -3,41 +3,41 @@ import sys
 import sys
 import numpy
 
-def heights(num, vecs):
-    height = [0 for i in range(num)]
-    heig = 0
-    for i in range(num):
+def heights(n, vecs):
+    possible_heights = [0 for i in range(n)]
+    max_height = 0
+     for i in range(n):
         height = 0
         p = i
         while not (int(vecs[p]) == -1):
-            if (height[p] != 0):
-                height += height[p] 
+            if (possible_heights[p] != 0):
+                height += possible_heights[p] 
                 break
             height += 1
             p = int(vecs[p])
-        height[i] = height
-        height = max(heig, height[i])
-    return heig + 1
+        possible_heights[i] = height
+        max_height = max(max_height, possible_heights[i])
+    return max_height + 1
 
 def main():
     command = input()
     if "F" in command:
         file_name = input()
-        pa = "test/" + file_name
+        path = "test/" + file_name
         if not "a" in file_name:
-            with open(pa, "r") as f:
+            with open(path, "r") as f:
                 text = f.read()
             partitioned = text.partition("\n")
-            num = int(partitioned[0])
+            n = int(partitioned[0])
             r = partitioned[2].split(" ")
             r = numpy.array(r)
-            print(heights(num, r))
+            print(heights(n, r))
     elif "I" in command:
-        num = int(input())
+        n = int(input())
         vecs = input()
         r = vecs.split(" ")
         r = numpy.array(r)
-        print(heights(num, r))
+        print(heights(n, r))
 
 if __name__ == "__main__":
     main()
