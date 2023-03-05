ee236dcd4b87d4c7d2b2e83c6f238cfc29739e41
ArtemijsZaharovs <124812244+ArtemijsZaharovs@users.noreply.github.com>
2023-03-05 19:00:19 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,7 +2,7 @@ import os
 
 def inputs():
     while True:
-        method = input("(F for file, I for keyboard): ")
+        method = input("(F for file, K for keyboard): ")
         if method.upper() == "F":
             filename = input("Enter filename: ")
             if "a" in filename:
@@ -12,9 +12,9 @@ def inputs():
                     return f.read().strip().split("\n")[1:]
             else:
                 print("File not found. ")
-        elif method.upper() == "I":
-            n = int(input("Enter  "))
-            vec = list(map(int, input("Enter  ").split()))
+        elif method.upper() == "K":
+            n = int(input("Enter number of nodes: "))
+            vec = list(map(int, input("Enter parents of nodes: ").split()))
             return n, vec
         else:
             print("Invalid input method. ")
