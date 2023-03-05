ba956621009ee5358ee4d3e07001b24b2b4ac324
ArtemijsZaharovs <124812244+ArtemijsZaharovs@users.noreply.github.com>
2023-03-05 17:46:56 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,21 +1,45 @@
-# python3
-def depth(uz, vec):
-    if vec[uz] == -1:
-        return 1
+import sys
+
+class Node:
+def init(self, n):
+self.n = n
+self.vec = []
+self.height = None
+
+def heights(n, vecs):
+uz = [Node(i) for i in range(n)]
+r = None
+for i, p in enumerate(vecs):
+    if p == -1:
+        r = uz[i]
     else:
-        return 1 + depth(vec[uz], vec)
+        uz[p].children.append(uz[i])
 
-def heights(k, vec):
-    height = 0
-    for i in range(k):
-        height = max(height, depth(i, vec))
-    return height
+if r is None:
+    return 0
 
-k = int(input())
-vec = list(map(int, input().split()))
+sx = [(r, 1)]
+max_height = 0
+while sx:
+    node, height = sx.pop()
+    node.height = height
+    max_height = max(max_height, height)
+    for child in node.children:
+        sx.append((child, height + 1))
 
-print(heights(k, vec))
+return max_height
+def main():
+text = input()
+if 'F' in text and not 'a' in text:
+name = input()
+file = "./test/" + name
+with open(file) as f:
+cip = f.readline()
+text = f.readline()
+if 'I' in text:
+cip = input()
+text = input()
+print(heights(int(cip), list(map(int, text.split()))))
 
-
-
-
+sys.setrecursionlimit(10**7)
+main()
